07fb612a3496693fd3e9b2c86d05dd9a76a1da00
Zareen Price <yazatamorph@gmail.com>
2021-08-01 22:12:50 -0400

    Server listens for and acts on read-receipt socket events.

--- client/src/store/utils/thunkCreators.js
+++ client/src/store/utils/thunkCreators.js
@@ -92,26 +92,23 @@ const sendMessage = (data, body) => {
   });
 };
 
-const sendReadReceipt = async (conversationId, messageId) => {
-  // TODO: Write API endpoint to update message read status
-  await axios.post("/api/read", { conversationId, messageId });
+const sendReadReceipt = (conversationId, messageId) => {
   socket.emit("read-receipt", {
     conversationId,
     messageId,
   });
 };
 
-export const updateReadReceipt =
-  (conversationId, messageId) => async (dispatch) => {
-    try {
-      // emits read receipt when user looks at new message
-      await sendReadReceipt(conversationId, messageId);
-      // updates when user reads message AND/OR receives a read receipt
-      dispatch(setMessageRead(conversationId, messageId));
-    } catch (err) {
-      console.error(err);
-    }
-  };
+export const updateReadReceipt = (conversationId, messageId) => (dispatch) => {
+  try {
+    // emits read receipt when user looks at new message
+    sendReadReceipt(conversationId, messageId);
+    // updates when user reads message
+    dispatch(setMessageRead(conversationId, messageId));
+  } catch (err) {
+    console.error(err);
+  }
+};
 
 // message format to send: {recipientId, text, conversationId}
 // conversationId will be set to null if its a brand new conversation
--- server/db/models/message.js
+++ server/db/models/message.js
@@ -10,6 +10,27 @@ const Message = db.define("message", {
     type: Sequelize.INTEGER,
     allowNull: false,
   },
+  viewed: {
+    type: Sequelize.BOOLEAN,
+    allowNull: false,
+    defaultValue: false,
+  },
 });
+
+Message.updateReadReceipt = async function (messageId) {
+  const message = await Message.findOne({
+    where: {
+      id: messageId,
+    },
+  });
+  // if record exists, update and save
+  if (message) {
+    message.viewed = true;
+    await message.save();
+  }
+
+  // return message or null if it doesn't exist
+  return message;
+};
 
 module.exports = Message;
