f1a928b170bdc8ec893971ba1ae1f739be91dca1
Zareen Price <yazatamorph@gmail.com>
2021-08-06 13:24:26 -0400

    Cleaned up unused class.

--- client/src/components/Sidebar/CountUnread.js
+++ client/src/components/Sidebar/CountUnread.js
@@ -3,17 +3,6 @@ import { Box, Typography } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles(() => ({
-  root: {
-    borderRadius: 8,
-    height: 80,
-    boxShadow: "0 2px 10px 0 rgba(88,133,196,0.05)",
-    marginBottom: 10,
-    display: "flex",
-    alignItems: "center",
-    "&:hover": {
-      cursor: "grab",
-    },
-  },
   bubble: {
     height: 25,
     width: 25,
